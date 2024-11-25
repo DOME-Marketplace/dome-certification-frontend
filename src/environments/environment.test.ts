bfa7ce6f4f5041a2af088ea0354d5e6d20ac3a49
Antonio Alvarez Lopez <antonio.alvarez@dekra.com>
2024-11-25 12:48:21 +0100

    update did

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -3,6 +3,6 @@ export const environment = {
   production: false,
   API_URL: 'https://dome-certification-api.dome-marketplace-dev2.org',
   VERIFIER_URL: 'https://verifier.dome-marketplace-dev2.org',
-  CLIENT_ID: 'did:key:zDnaehmkEhoybLgRkVbKpAv47Vu81Rw5TmLUA5PrRKuWSbhxn',
+  CLIENT_ID: 'did:key:zDnaemuFzGnbeSpxGUKmXMo354Nhvivfao3UjrNqLyJKX34gw',
   REQUEST_URI: "https://dome-certification-api.dome-marketplace-dev2.org/auth/oauth-token"
 };
