414c05e79c324a55d01497c2468ad493e5e5cbf8
Francisco José Ruiz Ojeda <francisco.ruiz@dekra.com>
2025-12-17 13:30:42 +0100

    new client_id for sbx

--- src/environments/environment.sbx.ts
+++ src/environments/environment.sbx.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: false,
   API_URL: 'https://dome-certification-api.dome-marketplace-sbx.org',
   VERIFIER_URL: 'https://verifier.dome-marketplace-sbx.org',
-  CLIENT_ID: 'did:key:zDnaehmkEhoybLgRkVbKpAv47Vu81Rw5TmLUA5PrRKuWSbhxn',
+  CLIENT_ID: 'did:key:zDnaep7iGodkmSEUD8uAsp4dhqcaxxrFQ3zgyANHD2brt7CgB',
   REQUEST_URI:
     'https://dome-certification-api.dome-marketplace-sbx.org/auth/oauth-token',
   REDIRECT_URI:
