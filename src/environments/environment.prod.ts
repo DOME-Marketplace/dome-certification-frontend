d5f7b43774cf39293ffa53ba0b819f0e3fbc23e7
Francisco José Ruiz Ojeda <francisco.ruiz@dekra.com>
2026-01-15 16:32:43 +0100

    new client id prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   API_URL: "https://dome-certification-api.dome-marketplace.eu",
   VERIFIER_URL: "https://verifier.dome-marketplace.eu",
-  CLIENT_ID: "did:key:zDnaehmkEhoybLgRkVbKpAv47Vu81Rw5TmLUA5PrRKuWSbhxn",
+  CLIENT_ID: "did:key:zDnaeRQmf6te2Gnoo1xQetePbyJfh2kLaHnYNr5z5PbSMWXL5",
   REQUEST_URI:
     "https://dome-certification-api.dome-marketplace.eu/auth/oauth-token",
   REDIRECT_URI: "https://dome-certification.dome-marketplace.eu/auth/login",
